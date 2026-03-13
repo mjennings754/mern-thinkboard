0d1642eb64e01b318e9fbd70c2671ce79c47160b
mjennings754 <mitchellj@outlook.jp>
2026-03-14 06:35:14 +1100

    add middleware (REDIS)

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,22 +2,30 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5001
-connectDB();
 
-// middleware
-app.use(express.json())
+
+// middleware allows us to get access to the title, content (we send as JSON)
+app.use(express.json()); // parse JSON bodies: req.body
+app.use(rateLimiter)
+
+app.use((req, res, next) => {
+    console.log(`Request method is ${req.method} & Request URL is ${req.url}`);
+    next();
+});
+
 app.use("/api/notes", notesRoutes);
 
 // An endpoint is a combination of url + http method that lets the client
 // interact with a specific resource
 
-
-app.listen(PORT, () => {
-    console.log("Server started on PORT:", PORT)
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on PORT:", PORT)
+    });
 });
-
